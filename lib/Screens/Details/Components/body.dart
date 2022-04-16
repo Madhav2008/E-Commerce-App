d4488221bfe3b68e4c7ca61b01b05452f87d368a
arora <aroramadhav65@gmail.com>
2022-04-16 23:26:03 +0530

    Update

--- lib/Screens/Details/Components/body.dart
+++ lib/Screens/Details/Components/body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/constants.dart';
-import 'package:e_commerce_app/models/Product.dart';
+import 'package:e_commerce_app/Models/Product.dart';
 import 'add_to_cart.dart';
 import 'color_and_size.dart';
 import 'counter_with_fav_btn.dart';
