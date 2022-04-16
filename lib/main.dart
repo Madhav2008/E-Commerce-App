3bd884c46901ebcd52633c8e66e816d485031314
arora <aroramadhav65@gmail.com>
2022-04-16 23:02:39 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/constants.dart';
-import 'package:shop_app/screens/home/home_screen.dart';
+import 'package:e_commerce_app/constants.dart';
+import 'package:e_commerce_app/screens/home/home_screen.dart';
 
 void main() {
   runApp(
