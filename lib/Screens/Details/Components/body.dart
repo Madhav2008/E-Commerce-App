3afdb725c75c473314d3a87b887011a9eacc41d7
arora <aroramadhav65@gmail.com>
2022-04-16 23:26:13 +0530

    Update

--- lib/Screens/Details/Components/body.dart
+++ lib/Screens/Details/Components/body.dart
@@ -10,7 +10,7 @@ import 'product_title_with_image.dart';
 class Body extends StatelessWidget {
   final Product product;
 
-  const Body({Key? key, this.product}) : super(key: key);
+  const Body({Key? key, required this.product}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     // It provide us total height and width
