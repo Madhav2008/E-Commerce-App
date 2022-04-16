c1373996035e6be7bc85c813531ae7953279d175
arora <aroramadhav65@gmail.com>
2022-04-16 23:31:02 +0530

    Update

--- lib/Screens/Details/Components/description.dart
+++ lib/Screens/Details/Components/description.dart
@@ -13,7 +13,7 @@ class Description extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin),
+      padding: EdgeInsets.symmetric(vertical: kDefaultPaddin),
       child: Text(
         product.description,
         style: TextStyle(height: 1.5),
