e14091c154936189cd44defc591575f12ddb3a63
arora <aroramadhav65@gmail.com>
2022-04-16 23:38:13 +0530

    Update

--- lib/Screens/Details/Details_Screen.dart
+++ lib/Screens/Details/Details_Screen.dart
@@ -10,7 +10,7 @@ class DetailsScreen extends StatelessWidget {
   final Product product;
 
   const DetailsScreen({Key? key, required this.product}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
