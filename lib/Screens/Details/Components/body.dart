1e0bd808ea2ace9ba5d671f6aaf0a7f5e1d731bb
arora <aroramadhav65@gmail.com>
2022-04-16 23:43:49 +0530

    Update

--- lib/Screens/Details/Components/body.dart
+++ lib/Screens/Details/Components/body.dart
@@ -10,7 +10,7 @@ import 'product_title_with_image.dart';
 class Body extends StatelessWidget {
   final Product product;
 
-  const Body({Key? key, required this.product}) : super(key: key);
+  Body({Key? key, required this.product}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
