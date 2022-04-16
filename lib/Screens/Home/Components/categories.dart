b95d241f69fa278355782bd8e83f8c4c73cdd4c3
arora <aroramadhav65@gmail.com>
2022-04-16 23:09:32 +0530

    Update

--- lib/Screens/Home/Components/categories.dart
+++ lib/Screens/Home/Components/categories.dart
@@ -12,7 +12,7 @@ class _CategoriesState extends State<Categories> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin),
+      padding: EdgeInsets.symmetric(vertical: kDefaultPaddin),
       child: SizedBox(
         height: 25,
         child: ListView.builder(
