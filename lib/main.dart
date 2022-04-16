1bb10ffa2ef767f2b9c112b504460134e4014382
arora <aroramadhav65@gmail.com>
2022-04-16 22:48:41 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp(),);
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);@override
+  MyApp({Key? key}) : super(key: key);
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
