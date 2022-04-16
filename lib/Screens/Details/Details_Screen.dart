9fc5ed82e26848d93e72373db2efc2fc933032d7
arora <aroramadhav65@gmail.com>
2022-04-16 23:15:59 +0530

    Update

--- lib/Screens/Details/Details_Screen.dart
+++ lib/Screens/Details/Details_Screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:shop_app/constants.dart';
 import 'package:shop_app/models/Product.dart';
-import 'package:shop_app/screens/details/components/body.dart';
+import 'package:e_commerce_app/screens/details/components/body.dart';
 
 class DetailsScreen extends StatelessWidget {
   final Product product;
