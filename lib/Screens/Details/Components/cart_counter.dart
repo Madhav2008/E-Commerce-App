249b21c4cf73b275dae6e3a4d80073119d5fa609
arora <aroramadhav65@gmail.com>
2022-04-16 23:27:36 +0530

    Update

--- lib/Screens/Details/Components/cart_counter.dart
+++ lib/Screens/Details/Components/cart_counter.dart
@@ -42,7 +42,7 @@ class _CartCounterState extends State<CartCounter> {
     );
   }
 
-  SizedBox buildOutlineButton({IconData icon, Function press}) {
+  SizedBox buildOutlineButton({required IconData icon, Function press}) {
     return SizedBox(
       width: 40,
       height: 32,
