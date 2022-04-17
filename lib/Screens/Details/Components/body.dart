42ef43d6de8a4b546ebebc9e0ca2dba9784abd70
arora <aroramadhav65@gmail.com>
2022-04-17 12:38:01 +0530

    Update

--- lib/Screens/Details/Components/body.dart
+++ lib/Screens/Details/Components/body.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors_in_immutables
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:e_commerce_app/constants.dart';
