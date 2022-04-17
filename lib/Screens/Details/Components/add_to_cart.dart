6875a859eba9015ac5ec31d1805d6523ca2be428
arora <aroramadhav65@gmail.com>
2022-04-17 12:36:43 +0530

    Update

--- lib/Screens/Details/Components/add_to_cart.dart
+++ lib/Screens/Details/Components/add_to_cart.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:e_commerce_app/Models/Product.dart';
