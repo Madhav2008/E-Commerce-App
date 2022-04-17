765399206746b2cca24e59138a5de5c355534073
arora <aroramadhav65@gmail.com>
2022-04-17 12:37:46 +0530

    Update

--- lib/Screens/Details/Components/body.dart
+++ lib/Screens/Details/Components/body.dart
@@ -12,7 +12,7 @@ import 'product_title_with_image.dart';
 class Body extends StatelessWidget {
   final Product product;
 
-  Body({Key? key, required this.product}) : super(key: key);
+  Body({Key? key, required this.product,}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
