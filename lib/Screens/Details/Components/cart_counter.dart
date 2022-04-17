d6081d9f990c199e2801a45cd768b67148bc6ce1
arora <aroramadhav65@gmail.com>
2022-04-17 12:39:52 +0530

    Update

--- lib/Screens/Details/Components/cart_counter.dart
+++ lib/Screens/Details/Components/cart_counter.dart
@@ -25,8 +25,7 @@ class _CartCounterState extends State<CartCounter> {
         Padding(
           padding: EdgeInsets.symmetric(horizontal: kDefaultPaddin / 2),
           child: Text(
-            // if our item is less  then 10 then  it shows 01 02 like that
-            numOfItems.toString().padLeft(2, "0"),
+           numOfItems.toString().padLeft(2, "0"),
             style: Theme.of(context).textTheme.headline6,
           ),
         ),
