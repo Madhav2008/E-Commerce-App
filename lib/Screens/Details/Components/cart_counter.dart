50da3d7cb9b83578d24f8a37626c650cb1d0ada8
arora <aroramadhav65@gmail.com>
2022-04-17 12:40:29 +0530

    Update

--- lib/Screens/Details/Components/cart_counter.dart
+++ lib/Screens/Details/Components/cart_counter.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
 
 import 'package:flutter/material.dart';
 import '../../../constants.dart';
